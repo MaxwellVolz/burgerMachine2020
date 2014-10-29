09a044ca6ce40d70ac6c83257975210729a23450
Troy <troydub@gmail.com>
2014-10-29 17:43:45 -0600

    2ndscript

--- ordersDB.js
+++ ordersDB.js
@@ -6,13 +6,13 @@
 
 //Day 1
 var a1text = [
-    "Yo. Give me that Large Fry and Large Drink. Aw yeah.",
+    "Give me that Large Fry and Large Drink. Aw yeah.",
     "Hey. Can I get a Large Fries and a Cheeseburger?",
-    "Whoa. Ok. I'll have one Cheeseburger with Tomato and a Medium Drink",
+    "Whoa. Ok. Cheeseburger with Tomato and a Medium Drink.",
     "Burger Machine? One Small Drink please.",
-    "Hello? Yeah, give me a Double Cheeseburger with Double Pickles. Thanks.",
-    "Can I have a Double Patty Burger with a Large Fry and Large Drink?",
-    "Hi. One Burger with Everything."];
+    "Hi. One Burger with Everything.",
+    "Can I have a Double Patty Burger with a Large Fry?",
+    "Hello? Yeah, give me a Double Cheeseburger. Thanks."];
 
 var a1order = [{
     cheese: "0",
@@ -43,64 +43,50 @@ var a1order = [{
     pickles: "0",
     tomatoes: "0"},
     {
-    cheese: "2",
-    drink: "",
-    fry: "",
-    patties: "2",
-    pickles: "2",
-    tomatoes: "0"},
-    {
-    cheese: "0",
-    drink: "large",
-    fry: "large",
-    patties: "2",
-    pickles: "0",
-    tomatoes: "0"},
-    {
     cheese: "1",
     drink: "",
     fry: "",
     patties: "1",
     pickles: "1",
-    tomatoes: "1"
+    tomatoes: "1"},
+    {
+    cheese: "0",
+    drink: "",
+    fry: "large",
+    patties: "2",
+    pickles: "0",
+    tomatoes: "0"},
+    {
+    cheese: "0",
+    drink: "",
+    fry: "",
+    patties: "2",
+    pickles: "0",
+    tomatoes: "0"
 }];
 
 //Day 2
 var a2text = [
-    "Yo. Give me that Large Fry and Large Drink. Aw yeah.",
-    "Hey. Can I get a Large Fries and a Cheeseburger?",
-    "Whoa. Ok. I'll have one Cheeseburger with Tomato and a Medium Drink",
-    "Burger Machine? One Small Drink please.",
-    "Hello? Yeah, give me a Double Cheeseburger with Double Pickles. Thanks.",
-    "Can I have a Double Patty Burger with a Large Fry and Large Drink?",
-    "Hi. One Burger with Everything."];
+    "Hi. I would like a Triple Cheeseburger.",
+    "Hey. Can I get a Small Drink and a Small Fry and a Cheeseburger?",
+    "One Double Cheeseburger with Everything.",
+    "This is cool. Just a Large Fry and a Burger with Double Pickles please.",
+    "Burger. Everything on it. Regular Drink and Fries.",
+    "Yo. Let me have a Burger with Two Pickle and Two Tomato",
+    "Can I get some Medium Fries a Plain Burger with a Large Drink. Thanks."];
 
 var a2order = [{
-    cheese: "0",
-    drink: "large",
-    fry: "large",
-    patties: "0",
+    cheese: "3",
+    drink: "",
+    fry: "",
+    patties: "3",
     pickles: "0",
     tomatoes: "0"},
     {
         cheese: "1",
-        drink: "",
-        fry: "large",
-        patties: "1",
-        pickles: "0",
-        tomatoes: "0"},
-    {
-        cheese: "1",
-        drink: "medium",
-        fry: "",
-        patties: "1",
-        pickles: "0",
-        tomatoes: "1"},
-    {
-        cheese: "0",
         drink: "small",
-        fry: "",
-        patties: "0",
+        fry: "small",
+        patties: "1",
         pickles: "0",
         tomatoes: "0"},
     {
@@ -108,20 +94,34 @@ var a2order = [{
         drink: "",
         fry: "",
         patties: "2",
+        pickles: "1",
+        tomatoes: "1"},
+    {
+        cheese: "0",
+        drink: "",
+        fry: "large",
+        patties: "1",
         pickles: "2",
         tomatoes: "0"},
     {
-        cheese: "0",
-        drink: "large",
-        fry: "large",
-        patties: "2",
-        pickles: "0",
-        tomatoes: "0"},
-    {
         cheese: "1",
+        drink: "medium",
+        fry: "medium",
+        patties: "1",
+        pickles: "1",
+        tomatoes: "1"},
+    {
+        cheese: "0",
         drink: "",
         fry: "",
         patties: "1",
-        pickles: "1",
-        tomatoes: "1"
+        pickles: "2",
+        tomatoes: "2"},
+    {
+        cheese: "0",
+        drink: "large",
+        fry: "medium",
+        patties: "1",
+        pickles: "0",
+        tomatoes: "0"
     }];
