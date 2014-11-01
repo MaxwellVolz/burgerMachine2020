95e13b2d1e8f7959dccd9e9b877ef4d21f1a6692
Troy <troydub@gmail.com>
2014-11-01 16:36:09 -0600

    3rd script

--- ordersDB.js
+++ ordersDB.js
@@ -70,7 +70,7 @@ var a2text = [
     "Hi. I would like a Triple Cheeseburger.",
     "Hey. Can I get a Small Drink and a Small Fry and a Cheeseburger?",
     "One Double Cheeseburger with Everything.",
-    "This is cool. Just a Large Fry and a Burger with Double Pickles please.",
+    "Cool! Just a Large Fry and a Burger with Double Pickles please.",
     "Burger. Everything on it. Regular Drink and Fries.",
     "Yo. Let me have a Burger with Two Pickle and Two Tomato",
     "Can I get some Medium Fries a Plain Burger with a Large Drink. Thanks."];
@@ -124,4 +124,65 @@ var a2order = [{
         patties: "1",
         pickles: "0",
         tomatoes: "0"
+    }];
+	
+	//Day 3
+var a3text = [
+    "Hi. I would like a Triple Cheeseburger with Pickles.",
+    "Double Double Burger. That's it.",
+    "Hey. Hey. One Small Fry. One Small Drink. One Burger. Please",
+    "Yo. Can I have a Burger with Double Cheese and Pickles and Double Tomato.",
+    "I would like a Burger with As Many Pickles as you can fit on it.",
+    "One Quad Cheeseburger with Small Fries.",
+    "I want a Large Fry, a Large Drink, and a Triple Burger with Triple Pickle and Triple Tomato."];
+
+var a3order = [{
+    cheese: "3",
+    drink: "",
+    fry: "",
+    patties: "3",
+    pickles: "1",
+    tomatoes: "0"},
+    {
+        cheese: "2",
+        drink: "",
+        fry: "",
+        patties: "2",
+        pickles: "0",
+        tomatoes: "0"},
+    {
+        cheese: "0",
+        drink: "small",
+        fry: "small",
+        patties: "1",
+        pickles: "0",
+        tomatoes: "0"},
+    {
+        cheese: "2",
+        drink: "",
+        fry: "",
+        patties: "1",
+        pickles: "1",
+        tomatoes: "2"},
+    {
+        cheese: "0",
+        drink: "",
+        fry: "",
+        patties: "1",
+        pickles: "4",
+        tomatoes: "0"},
+    {
+        cheese: "4",
+        drink: "",
+        fry: "small",
+        patties: "4",
+        pickles: "0",
+        tomatoes: "0"},
+    {
+        cheese: "0",
+        drink: "large",
+        fry: "large",
+        patties: "3",
+        pickles: "3",
+        tomatoes: "3"
     }];
